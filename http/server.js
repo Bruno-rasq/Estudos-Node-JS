05cdbe8a1339397cc5e18d8ee24a34e2541d9b14
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-09-05 18:45:28 -0300

    vou precisar ver com mais calma esses scripts ainda

--- http/server.js
+++ http/server.js
@@ -4,20 +4,39 @@ const path = require('path');
 
 http.createServer((req, res) => {
 
-    if(req.url === '/'){
-       fs.readFile(
-        path.join(__dirname, 'public', 'index.html'),
+    const file = req.url === '/' ? 'index.html' : req.url
+    const filePath = path.join(__dirname, 'public', file);
+    const extname = path.extname(filePath)
+
+    const allowedFileTypes = ['.html', '.css', '.js'];
+    const allowed = allowedFileTypes.find(item => item === extname)
+
+    if(!allowed) return
+
+    fs.readFile(
+        filePath,
         (err, content) => {
 
-            if(err) throw err
+            if (err) throw err
 
             res.end(content)
         }
-       )
-    }
+    )
 
-    if(req.url === '/contatos'){
-        return res.end('<h1>Contact Page</h1>')
-    }
+    // if(req.url === '/'){
+    //    fs.readFile(
+    //     path.join(__dirname, 'public', 'index.html'),
+    //     (err, content) => {
+
+    //         if(err) throw err
+
+    //         res.end(content)
+    //     }
+    //    )
+    // }
+
+    // if(req.url === '/contatos'){
+    //     return res.end('<h1>Contact Page</h1>')
+    // }
 
 }).listen(5000, () => console.log('server run'))
