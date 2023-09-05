0fb6c69883832f3b6b75b7c04882a0b8bb7d8e4f
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-09-05 19:46:11 -0300

    api concluida

--- http/API/index.js
+++ http/API/index.js
@@ -1,8 +1,42 @@
 const http = require('http');
+const URL = require('url');
+const fs = require('fs')
+const path = require('path');
+
 const data = require('./urls.json');
+
+
+function writeFile(cb){
+    fs.writeFile(
+        path.join(__dirname, "urls.json"),
+        JSON.stringify(data, null, 2),
+        err => {
+            if (err) throw err
+
+            cb(JSON.stringify({ message: "OK" }))
+        }
+
+    )
+}
 
 http.createServer((req, res) => {
 
-    res.end(JSON.stringify(data))
+    const { name, url, del } = URL.parse(req.url, true).query
 
-}).listen(3000, () => console.log('server is running...'))
+    // all resources
+    if (!name || !url) {
+        return res.end(JSON.stringify(data))
+    }
+
+    if (del) {
+        data.urls = data.urls.filter(item => String(item.url) !== String(url))
+        writeFile((message) => res.end(message))
+
+        return 
+    }
+
+    data.urls.push({name, url})
+
+    return writeFile((message) => res.end(message))
+
+}).listen(3000, () => console.log('API is running...'))
