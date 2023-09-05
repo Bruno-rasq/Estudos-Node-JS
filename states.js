bf1aaaeac77be73c1d6e35ddc3be2e14d2ce34cd
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-09-05 16:27:04 -0300

    states js

---
+++ states.js
@@ -0,0 +1,23 @@
+const os = require('os');
+
+setInterval(() => {
+
+    const { freemem, totalmem } = os;
+    
+    const mem = parseInt(freemem() /1024 / 1024);
+    const total = parseInt(totalmem() /1024 / 1024);
+    const percents = parseInt((mem / total) * 100);
+    
+    const states = {
+        free: `${mem}MB`,
+        total: `${total}MB`,
+        usage: `${percents}%`
+    };
+    
+    console.clear();
+    console.log("=== PC stats ===");
+    console.table(states);
+
+}, 1000);
+
+
